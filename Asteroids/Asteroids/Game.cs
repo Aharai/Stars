e2993fc1122c075fd4e91ccb478bc84fd7d34ca5
Stanislav Ponomarev <freezak_36rus@mail.ru>
2019-12-22 22:33:01 +0300

    lesson 1

---
+++ Asteroids/Asteroids/Game.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Asteroids
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        static Game()
+        {
+        }
+
+        public static BaseObject[] _objs;
+        public static void Load()
+        {
+            _objs = new BaseObject[40];
+            for (int i = 0; i < _objs.Length / 2; i++)
+                _objs[i] = new BaseObject(new Point(600, i * 20), new Point(-i, -i), new Size(10, 10));
+
+            for (int i = _objs.Length / 2; i < _objs.Length; i++)
+                _objs[i] = new Star(new Point(600, i * 20), new Point(-i, 0), new Size(5, 5));
+
+            for (int i = 0; i < 5; i++)
+                _objs[i] = new Star2(new Point(600, i * 20), new Point(-i, 0), new Size(5, 5));
+
+
+            for (int i = 0; i < 10; i++)
+                _objs[i] = new Star3(new Point(600, i * 20), new Point(-i, 0), new Size(5, 5));
+        }
+
+        public static void Init(Form form)
+        {
+
+            Load();
+          
+            Graphics g;
+
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+        }
+        public static void Draw()
+        {
+
+            Buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
+            Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+
+    }
+}
